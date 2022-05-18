4efa9c2d6ba962525daf13c1c94a3b154792fb36
Minjune Hwang <mjhwang@berkeley.edu>
2022-05-17 20:03:15 -0700

    Update job scripts

--- HSAM.sh
+++ HSAM.sh
@@ -1,6 +1,6 @@
 for SEED in 1 2 3
 do
-    for MAX_DUMMIES in 0 20 75
+    for MAX_DUMMIES in 0 75
     do
         python train.py --model HSAM --min_dummies 0 --max_dummies $MAX_DUMMIES --seed $SEED
     done
